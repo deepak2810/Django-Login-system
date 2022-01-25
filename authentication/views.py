dc674cbfa426a5887f4146740bf569dc9ed12f17
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-25 23:00:02 +0530

    django login system

---
+++ authentication/views.py
@@ -0,0 +1,147 @@
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+import email
+from email import message
+from readline import get_current_history_length
+from tokenize import generate_tokens
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from gfg.info import EMAIL_HOST, EMAIL_HOST_USER
+from gfg import settings
+from . import views
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+
+
+
+
+# Create your views here.
+def home(request):
+    return render(request,"authentication/index.html")
+
+def signup(request):
+
+
+    if request.method == "POST":
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email=request.POST['email']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist. Please try some other username")
+            return redirect('home')
+        if User.objects.filter(email=email):
+            messages.error(request,"Email already registered")
+            return redirect('home') 
+
+        if len(username)>10:
+            messages.error(request,"Username must be under 10 characters.")
+            return redirect('home')
+
+        if pass1 != pass2:
+            messages.error(request,"Passwords didn't match.")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request,"Username must be alphanumeric.")
+            return redirect('home')            
+
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.is_active = False
+
+        myuser.save()
+
+        messages.success(request,"Your account has been successfully created. we've also sent you an confirmation email, please confirm your email in order to confirm your account.")
+
+        #WELCOME EMAIL
+        subject = "welcome to LOGIN SYSTEM by deepak"
+        message="hello "+myuser.first_name + "!  \n" +"Welcome to LOGIN SYSTEM by deepak khanka. \n  We thank you for visiting our LOGIN SYSTEM.\n We have also sent you a confirmation email, please confirm your email address."
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently = True)
+
+
+        #email address confirmation email code.
+
+        current_site = get_current_history_length(request)
+        email_subject = "Confirm your mail @LOGIN_SYSTEM by Deepak."
+        message2= render_to_string('email_confirmation.html',
+        {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid':urlsafe_b64encode(force_bytes(myuser.pk)),
+            'token': generate_tokens.make_token()
+            })
+
+        email = message.EmailMessage(
+                email_subject,
+                message2,
+                settings.EMAIL_HOST_USER,
+                [myuser.email] 
+            )
+        email.fail_silently = True
+        email.send()
+
+
+
+
+        return redirect('signin')
+
+
+    return render(request,"authentication/signup.html")
+
+def signin(request):
+
+    if request.method == 'POST':
+        username= request.POST['username']
+        pass1= request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+
+        if user is not None:
+          login(request, user)
+          fname = user.first_name
+          return render(request,"authentication/index.html",{'fname':fname})
+
+        else:
+            messages.error(request,"Bad crededntials")
+            return redirect ('home')  
+
+
+
+
+
+    return render(request,"authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out successfully")
+    return redirect ('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_b64decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_tokens.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+
+    else:
+        return render(request, 'activation_failed.html')            
---
+++ gfg/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER ='deepakkhanka98@gmail.com'
+EMAIL_HOST_PASSWORD='Khanka98@gmail.com'
+EMAIL_PORT = 587
